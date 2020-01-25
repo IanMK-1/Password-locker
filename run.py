0b8270a45275ed59067e0e14660efd69a63627d3
ian mwangi <iankimotho233@gmail.com>
2020-01-25 15:57:56 +0300

    Request user login details

--- run.py
+++ run.py
@@ -54,45 +54,64 @@ def randomString(stringLength=10):
 
 def main():
     print("Hello, welcome to Password Locker Application. What is your name?")
-    print('\t')
     user_name = input()
 
     print(f"Hello {user_name}. what would you like to do?")
     print('\n')
 
-    while True:
-        print(
-            "USE THE FOLLOWING COMMAND TO CREATE ACCOUNT: "
-            "ca - create new password locker account, "
-            "ex - exit"
-        )
-        print('\t')
-        command = input().lower()
+    print(
+        "USE THE FOLLOWING COMMAND TO CREATE ACCOUNT: "
+        "ca - create new password locker account, "
+        "ex - exit"
+    )
+    print('\t')
+    command = input().lower()
 
-        if command == "ca":
-            print("New Account")
-            print("-" * 15)
+    if command == "ca":
+        print("New Account")
+        print("-" * 15)
 
-            print("First Name ...")
-            first_name = input()
+        print("First Name ...")
+        first_name = input()
 
-            print("Last Name ...")
-            last_name = input()
+        print("Last Name ...")
+        last_name = input()
 
-            print("Username ...")
-            username = input()
+        print("Username ...")
+        username = input()
 
-            print("Password ...")
-            password = input()
+        print("Password ...")
+        password = input()
 
-            save_user_details(create_user_account(first_name, last_name, username, password))
-            print('\n')
-            print(f"{first_name} {last_name}, New Account Created")
-            print('\n')
+        save_user_details(create_user_account(first_name, last_name, username, password))
+        print('\n')
+        print(f"{first_name} {last_name}, New Account Created")
+        print('\n')
 
-        elif command == "ex":
-            print("Goodbye..")
-            break
+    elif command == "ex":
+        print("Goodbye..")
+        exit()
+
+    else:
+        print("Goodbye..")
+        exit()
+
+    print("LOGIN:")
+    print("Please enter your Username and password:")
+
+    print("Username ..")
+    su_username = input()
+
+    print("Password ..")
+    su_password = input()
+
+    if check_existing_user(su_username, su_password):
+        print(f"Welcome {first_name}")
+
+    else:
+        print("\n")
+        print("Wrong Username or password")
+        print("Bye")
 
 
 if __name__ == '__main__':
