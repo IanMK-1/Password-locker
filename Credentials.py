5d387c884378212ca9fa79cab5e0d111eb7a19df
ian mwangi <iankimotho233@gmail.com>
2020-01-25 13:07:42 +0300

    Add test to get user account by username

--- Credentials.py
+++ Credentials.py
@@ -1,14 +1,22 @@
 class Credentials:
     """Class that instantiates the credentials class"""
 
-    credentials_list = {}
+    credentials_list = []
 
-    def __init__(self, f_name, l_name, email, username, password):
+    def __init__(self, account, f_name, l_name, email, username, password):
+        self.account = account
         self.f_name = f_name
         self.l_name = l_name
         self.email = email
         self.username = username
         self.password = password
 
-    def save_instagram_credentials(self):
-        Credentials.credentials_list.update({'instagram': self})
+    def save_credentials(self):
+        Credentials.credentials_list.append(self)
+
+    # @classmethod
+    # def get_account(cls, username):
+    #     for user_account in cls.credentials_list:
+    #         if user_account.username == username:
+    #             return user_account
+
--- credentials_test.py
+++ credentials_test.py
@@ -8,21 +8,43 @@ class TestCredentials(unittest.TestCase):
     def setUp(self) -> None:
         """Instantiate the properties of the object"""
 
-        self.new_credentials = Credentials("luke", "shaw", "joe@gmail.com", "jluke", "password")
+        self.new_credentials = Credentials("instagram", "luke", "shaw", "joe@gmail.com", "jluke", "password")
 
     def test_init(self):
+        """Test to check if the objects are being instantiated correctly"""
+
+        self.assertEqual(self.new_credentials.account, "instagram")
         self.assertEqual(self.new_credentials.f_name, "luke")
         self.assertEqual(self.new_credentials.l_name, "shaw")
         self.assertEqual(self.new_credentials.email, "joe@gmail.com")
         self.assertEqual(self.new_credentials.username, "jluke")
         self.assertEqual(self.new_credentials.password, "password")
 
-    def test_save_instagram_credentials(self):
+    def test_save_credentials(self):
         """Test_save_credentials is a test to show if user credentials are being saved"""
 
-        self.new_credentials.save_instagram_credentials()
+        self.new_credentials.save_credentials()
         self.assertEqual(len(Credentials.credentials_list), 1)
 
-    
+    def tearDown(self) -> None:
+        Credentials.credentials_list = []
+
+    def test_save_multiple_credentials(self):
+        """test_save_multiple_credentials test checks if multiple credentials are being saved"""
+
+        self.new_credentials.save_credentials()
+        test_credentials = Credentials("twitter", "john", "hayes", "jh@ymail.com", "jhayes", "mountain")
+        test_credentials.save_credentials()
+        self.assertEqual(len(Credentials.credentials_list), 2)
+
+    # def test_get_account_by_username(self):
+    #     self.new_credentials.save_credentials()
+    #     test_credentials = Credentials("twitter", "john", "hayes", "jh@ymail.com", "jhayes", "mountain")
+    #     test_credentials.save_credentials()
+    #
+    #     got_account = Credentials.get_account("jhayes")
+    #     self.assertEqual(got_account.f_name, test_credentials.f_name)
+
+
 if __name__ == '__main__':
     unittest.main()
